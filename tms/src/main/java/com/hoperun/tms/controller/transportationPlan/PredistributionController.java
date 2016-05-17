89a67cc2134c2a38b99fb5c50852a4b07053440a
zhan174056 <747174056@qq.com>
2016-05-17 14:04:15 +0800

    test

--- tms/src/main/java/com/hoperun/tms/controller/transportationPlan/PredistributionController.java
+++ tms/src/main/java/com/hoperun/tms/controller/transportationPlan/PredistributionController.java
@@ -67,7 +67,6 @@ public class PredistributionController {
 		}else{
 			criteria.andDeliveryNoEqualTo(deliveryNo);
 		}
-		aaaa
 		waybillExample.setOrderByClause("UPDATED_AT desc");
 		int pageNumber = 1;
 		int pageSize = 10;
